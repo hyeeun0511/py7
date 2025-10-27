1bbf1eb5b1baac914db9bf99615401c13ab70abd
hyeeun0511 <bang48188@naver.com>
2025-10-27 18:26:32 +0900

    수업

---
+++ p1025/p18_list.py
@@ -0,0 +1,26 @@
+# 이름을 추가하는 프로그램을 구현하시오. [] 홍길동, 유관순, 이순신
+name = []
+name2 = input("이름을 작성하세요.")
+name.append(name2)
+print("이름리스트 : ",name)
+
+name.append(input("이름을 작성하세요."))
+print("이름리스트 : ",name)
+
+name.append(input("이름을 작성하세요."))
+print("이름리스트 : ",name)
+
+
+# # 과일을 추가하는 프로그램을 구현하시오.
+
+# fruit = []
+# name = input("과일이름을 입력하세요.")
+# fruit.append(name)
+# print("과일리스트 : ",fruit)
+
+# name = input("과일이름을 입력하세요.")
+# fruit.append(name)
+# print("과일리스트 : ",fruit)
+
+# fruit.append(input("과일이름을 입력하세요."))
+# print("과일리스트 : ",fruit)
---
+++ p1025/p19_list.py
@@ -0,0 +1,36 @@
+# 이름, 국어, 영어, 수학, 합계, 평균
+
+# 3명의 학생성적을 입력받아 stus에 모두 저장하여 출력하시오.
+
+# stus = []
+# stu.append(['홍길동',100,100,100,300,100.0])
+
+stus = []
+name = input("이름을 입력하세요.")
+stus.append(name)
+kor = input(input("국어점수를 입력하세요."))
+stus.append(kor)
+eng = input(input("영어점수를 입력하세요."))
+stus.append(eng)
+math = input(input("수학점수를 입력하세요."))
+stus.append(math)
+total = kor+eng+math
+stus.append(total)
+avg = total/3
+stus.append(avg)  
+stus.append(total)  # 전체리스트
+
+# list = input("국어성적을 입력하세요.")
+# stus.append(list)
+# print("성적리스트 : ",stus)
+
+# list2 = input("영어성적을 입력하세요.")
+# stus.append(list2)
+# print("성적리스트 : ",stus)
+
+# list3 = input("수학성적을 입력하세요.")
+# stus.append(list3)
+# print("성적리스트 : ",stus)
+
+# stus.append(list)
+# print("성적리스트 : ",stus,"")
